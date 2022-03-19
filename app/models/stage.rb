50902a71dd2a1dbde941e435674a263a642d0d91
pandaulait <show.hey.panda@gmail.com>
2022-03-20 02:40:26 +0900

    admin edit

--- app/models/stage.rb
+++ app/models/stage.rb
@@ -4,5 +4,6 @@ class Stage < ApplicationRecord
   belongs_to :user
   validates :name, presence: true, length: { minimum: 1, maximum: 30 }
   validates :description, presence: true, length: { minimum: 1, maximum: 30 }
+  validates :image, presence: true
   mount_uploader :image, ImageUploader
 end

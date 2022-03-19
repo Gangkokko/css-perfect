36369db74a99f78fbbd4a2e330d37e7efd0d5927
pandaulait <show.hey.panda@gmail.com>
2022-03-19 17:45:43 +0900

    omniauth

---
+++ app/helpers/api/v1/auth/omniauth_callbacks_helper.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    module Auth
+      module OmniauthCallbacksHelper
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, { format: 'json' } do
     namespace :v1 do # バージョン1を表している
       resources :categories
+      # mount_devise_token_auth_for 'User', at: 'auth'
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        omniauth_callbacks: 'api/v1/auth/omniauth_callbacks'
+      }
     end
   end
 end

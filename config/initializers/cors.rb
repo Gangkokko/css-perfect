1867b08982441145a0e475f14d655bd9001fadae
pandaulait <show.hey.panda@gmail.com>
2022-03-19 16:10:47 +0900

    exposeを設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,10 +2,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000' # React側はポート番号3000で作るので「localhost:3000」を指定
+    origins "localhost:3000" # React側はポート番号3000で作るので「localhost:3000」を指定
 
-    resource '*',
-             headers: :any,
-             methods: %i[get post put patch delete options head]
+    resource "*",
+      headers: :any,
+      expose: ["access-token", "expiry", "token-type", "uid", "client"], # 追記
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
+
